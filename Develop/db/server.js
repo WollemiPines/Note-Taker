2ad32f31427fef1134a4c7d929c3d8f1950edb2f
Chris Wallace <Chrisw1096@gmail.com>
2022-07-27 18:06:51 +1000

    linked notes to index.html

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
 const api = require('../public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Import custom middleware, "cLog"
-// app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -22,7 +18,7 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for notes page
-app.get('/api/notes', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../notes.html'))
 );
 
