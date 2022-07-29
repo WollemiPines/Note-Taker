36f588fd850d2e69e933d1e4e7660d3c2772bbf7
Chris Wallace <Chrisw1096@gmail.com>
2022-07-29 13:32:24 +1000

    save bttn displaying correctly and css linked

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', require('./routes/api/routefile'));
